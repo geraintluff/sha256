0066b516453758e6d8f00352e7e147396bfde2ef
Geraint <luffgd@gmail.com>
2014-09-17 20:40:36 +0100

    Rename

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
 	var path = require('path');
 	var util = require('util');
 	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.initConfig({
 		mochaTest: {
@@ -15,9 +16,25 @@ module.exports = function (grunt) {
 					bail: false
 				}
 			}
+		},
+		uglify: {
+			main: {
+				options: {
+					report: 'min',
+				},
+				files: {
+					'sha256.min.js': ['sha256.js']
+				}
+			}
 		}
 	});
 	
-	grunt.registerTask('test', ['mochaTest']);
-	grunt.registerTask('default', ['test']);
+	grunt.registerTask('measure', function () {
+		var fs = require('fs');
+		var code = fs.readFileSync('sha256.js');
+		console.log('Minified length: ' + code.length + ' bytes');
+	});
+	
+	grunt.registerTask('test', ['mochaTest', 'uglify']);
+	grunt.registerTask('default', ['test', 'measure']);
 };
